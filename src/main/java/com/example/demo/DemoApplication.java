2f09cc27aab49ca9e5e1ab76558389415595186b
Weekender Association <csweekender@gmail.com>
2022-03-07 12:46:51 +0530

    Little change to test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,7 +16,7 @@ public class DemoApplication {
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return "Hello %s!"+name;
+        return "Hello "+name;
         //return String.format("Hello %s!", name);
     }
 
