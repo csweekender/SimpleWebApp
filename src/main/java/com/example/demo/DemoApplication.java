b20c7bb6c432771cd28bde4d652d1bc4987e8f99
Weekender Association <csweekender@gmail.com>
2022-03-25 17:57:47 +0530

    Fetching values from application.properties

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class DemoApplication {
 
+    @Autowired
+    private Environment environment;
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+
+
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return "Hello "+name;
+        String prop_value = environment.getProperty("my-custom-user");
+        return "Hello "+name+" fetching value from "+prop_value;
         //return String.format("Hello %s!", name);
     }
-
 }
